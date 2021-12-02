c89c31c57788e1fb44320d007c9ec2a811a09e87
mrcdk <contact@mrcdk.com>
2021-12-02 12:18:31 +0100

    Fix GC issues on iOS 15 with iPhone 12

--- project/src/backend/sdl/SDLApplication.cpp
+++ project/src/backend/sdl/SDLApplication.cpp
@@ -168,6 +168,14 @@ namespace lime {
 
 	void SDLApplication::HandleEvent (SDL_Event* event) {
 
+
+		#if defined(IPHONE) || defined(EMSCRIPTEN)
+		
+		int top = 0;
+		gc_set_top_of_stack(&top, false);
+
+		#endif
+
 		switch (event->type) {
 
 			case SDL_USEREVENT:
