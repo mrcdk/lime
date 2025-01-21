7c6503160cebdaa670af5e6b15c0b27ae1f18b9a
mrcdk <contact@mrcdk.com>
2025-01-21 11:10:34 +0100

    Updated SDL2 to version 2.30.11

--- project/lib/custom/sdl/include/SDL_config_linux.h
+++ project/lib/custom/sdl/include/SDL_config_linux.h
@@ -72,6 +72,7 @@
 #define HAVE_MATH_H 1
 #define HAVE_ICONV_H 1
 #define HAVE_SIGNAL_H 1
+#define HAVE_LINUX_INPUT_H 1
 /* #undef HAVE_ALTIVEC_H */
 /* #undef HAVE_PTHREAD_NP_H */
 /* #undef HAVE_LIBUDEV_H */
